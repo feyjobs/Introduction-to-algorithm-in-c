c14a5a0eb5309313a85be7e138735db2586d7687
gaohao <gaohao@iwaimai.baidu.com>
2017-10-26 01:15:56 +0800

    a

---
+++ Operation.h
@@ -0,0 +1,8 @@
+#include "Iostream.h"
+#include "Stack.h"
+#include "Basic.h"
+#include "BinaryTree.h"
+int getMax(int,int,int);
+int getMin(int,int,int);
+void exchange(int* ,int* );
+void initArray(int*);
